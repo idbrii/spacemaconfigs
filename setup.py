752f395ab557271d616794c3aa42d65d2a45bc43
David Briscoe <idbrii@gmail.com>
2015-12-04 12:31:54 -0800

    Fix spacemaconfig_path and update to work on Win32
    I think '__file__' only worked because I was running from the directory
    the script was in. It's actually a variable and not a string.
    
    Fix existence detection since it could be a directory and not a file:
    use exists() instead.
    
    Python doesn't have os.symlink on Windows. There are a bunch of
    solutions including ones that require changing permissions of
    python.exe. Instead, I'm using one that shells out to mklink and works
    when run from an admin command prompt (the standard mklink requirement).
    Source: http://stackoverflow.com/a/28382515/79125
    
    Tested in Win8.

--- setup.py
+++ setup.py
@@ -3,7 +3,23 @@ from __future__ import print_function
 
 import os
 
-spacemaconfig_path = os.path.dirname(os.path.realpath('__file__'))
+
+# Python doesn't have symlink support under windows. Add it, but this must be
+# run as admin.
+os_symlink = getattr(os, "symlink", None)
+if not callable(os_symlink):
+    def symlink_ms(source, link_name):
+        flag = ""
+        if os.path.isdir(source):
+            flag = "/D"
+        os.system("mklink {flags} {link} {target}".format(
+            flags = flag,
+            link = os.path.abspath(link_name),
+            target = os.path.abspath(source)))
+    os.symlink = symlink_ms
+
+
+spacemaconfig_path = os.path.dirname(os.path.realpath(__file__))
 
 def link_file_to_home(filename, needs_dot=True):
     config_file = os.path.join(spacemaconfig_path, filename)
@@ -15,9 +31,9 @@ def link_file_to_home(filename, needs_dot=True):
     print()
     print('{src}[{src_good}] to {dest}[{dest_good}]'.format(
         src=config_file,
-        src_good=os.path.isfile(config_file),
+        src_good=os.path.exists(config_file),
         dest=destination,
-        dest_good=os.path.isfile(destination)))
+        dest_good=os.path.exists(destination)))
     try:
         os.symlink(config_file, destination)
     except OSError as ex:
